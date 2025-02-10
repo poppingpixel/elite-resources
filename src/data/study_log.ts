5f27afae6db86ce43a2a0d4795b51fbb315d8e22
poppingpixel <71805235+poppingpixel@users.noreply.github.com>
2025-02-10 17:18:23 +0530

    feat: annotate "Proximal Policy Optimization Algorithms"

--- src/data/study_log.ts
+++ src/data/study_log.ts
@@ -97,4 +97,5 @@ export const STUDY_LOG: string[] = [
   "AD-DROP: Attribution-Driven Dropout for Robust Language Model Fine-Tuning [NLP, 2022]",
   "Mixture of Attention Heads: Selecting Attention Heads Per Token [NLP, 2025]",
   "Efficient Deep Learning: A Survey on Making Deep Learning Models Smaller, Faster, and Better [Core ML, 2021]",
+  "Proximal Policy Optimization Algorithms [RL, 2017]",
 ];
