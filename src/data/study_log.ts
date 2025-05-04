8cc181d578f6eb8e0b1b43a7a54df6eea072a217
poppingpixel <71805235+poppingpixel@users.noreply.github.com>
2025-05-04 16:42:11 +0530

    review: Selected Papers paper on NLP

--- src/data/study_log.ts
+++ src/data/study_log.ts
@@ -261,4 +261,5 @@ export const STUDY_LOG: string[] = [
   "MatchboxNet: 1D Time-Channel Separable Convolutional Neural Network Architecture for Speech Commands Recognition [Speech, 2020]",
   "Accelerating Large Language Model Decoding with Speculative Sampling [NLP, 2023]",
   "TextDiffuser-2: Unleashing the Power of Language Models for Text Rendering [Multimodal, 2023]",
+  "System 2 Attention (is something you might need too) [NLP, 2023]",
 ];
