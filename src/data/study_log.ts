c0ac033faa4f78fa786aa840a379b64471ce3784
poppingpixel <71805235+poppingpixel@users.noreply.github.com>
2025-08-31 20:26:42 +0530

    review: Seminal Papers paper on Computer Vision

--- src/data/study_log.ts
+++ src/data/study_log.ts
@@ -541,4 +541,5 @@ export const STUDY_LOG: string[] = [
   "VATT: Transformers for Multimodal Self-Supervised Learning from Raw Video, Audio and Text [Computer Vision, 2021]",
   "Visualizing and Understanding Convolutional Networks [Computer Vision, 2013]",
   "Denoising Diffusion Implicit Models [Computer Vision, 2022]",
+  "Conditional Negative Sampling for Contrastive Learning of Visual Representations [Computer Vision, 2020]",
 ];
