6689618e1bf38a846ac3d47bbb64c4839bee7140
poppingpixel <71805235+poppingpixel@users.noreply.github.com>
2026-01-09 12:51:25 +0530

    notes: summarize "Magicoder: Source Code Is All You Need"

--- src/data/study_log.ts
+++ src/data/study_log.ts
@@ -672,4 +672,5 @@ export const STUDY_LOG: string[] = [
   "VideoMAE: Masked Autoencoders are Data-Efficient Learners for Self-Supervised Video Pre-Training [Multimodal, 2022]",
   "Monarch Mixer: A Simple Sub-Quadratic GEMM-Based Architecture [NLP, 2023]",
   "LLM-Pruner: On the Structural Pruning of Large Language Models [NLP, 2023]",
+  "Magicoder: Source Code Is All You Need [NLP, 2023]",
 ];
